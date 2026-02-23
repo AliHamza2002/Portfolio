a851c5558c986a35569cc2153424729188f31a8e
AliHamza2002 <alihamzazubair@gmail.com>
2026-02-23 14:47:49 +0500

    Resume Added

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -85,7 +85,7 @@ export function Hero() {
               size="lg"
               className="gap-2 font-medium border-primary/30 hover:bg-primary/10 hover:text-primary"
             >
-              <a href="https://docs.google.com/document/d/1MprEc8YAQOtm08ofziVe_YliM1gwKkVolsIyTY2KKkg/edit?usp=sharing">View Resume</a>
+              <a href="https://drive.google.com/file/d/1A8eH3zTYFr8dNQYl--soYkdYvhgJC1US/view?usp=drive_link">View Resume</a>
             </Button>
           </div>
         </div>
